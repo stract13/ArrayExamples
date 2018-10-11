b449d949cc1eb6bd95535e1a8c86efa98f896ac5
stract13 <43944427+stract13@users.noreply.github.com>
2018-10-11 12:27:32 -0700

    change
    yes

--- Main.java
+++ Main.java
@@ -32,26 +32,37 @@ class Main
         System.out.println("*** PRINTING ar1 *");
         int[] ar1 = new int[n];
         
-        for 9int i=0 ; i<n; i++)
-            ar1{i}=1;
+        for (int i=0 ; i<n; i++)
+            ar1[i]=1;
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
         System.out.println("** ar2 **");
-        
+        int[] ar2;
+        ar2 = new int[ar1.length];
         for (int i=0 ; i<ar2.length ; i++)
+        {
             ar2[i]=ar1[i];
-            
+        }   
         for (int i=0 ; i<ar2.length ; i++)
+        {
             System.out.println("ar2[" +1+  "] = "+ar2[i]);
-        
+        }
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
-        
+         System.out.println("** Stask 3 **");
+
+         for (int i=0 ; i<ar1.length ; i++)
+         {
+             ar1[i] = ar1[i] + 1;
+         }
+
+         for (int i=0 ; i<ar1.length ; i++)
+         System.out.println("ar1[" + i + "] = "+ar1[i]);
 
         
         
@@ -62,6 +73,20 @@ class Main
          *         ar1: 1 2 3
          *         ar3: 1 2 3 0 1 2 3
          */
+        System.out.println("Task 4");
+                int[] ar3 = new int[ ar1.length *2 ];
+        for (int i=0 ; i< ar1.length ; i++)
+        {
+            ar3[i]=ar1[i];
+        }
+        
+        for (int i=0 ; i<ar1.length ; i++)
+        {
+            ar3[i+ar1.length]=ar1[i];
+        }
+        
+        for (int i=0 ; i<ar3.length ; i++)
+            System.out.println("ar3[" + i + "] = "+ar3[i]);
         
         
         /*
