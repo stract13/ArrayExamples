49254aa79e540f8b4e41d4b9f64a59fe557f23d6
stract13 <43944427+stract13@users.noreply.github.com>
2018-10-25 12:27:14 -0700

    dfs
    dfs

--- Main.java
+++ Main.java
@@ -232,7 +232,23 @@ class Main
         /*
          * Task 11.  Reverse the order of elements in ar2
          */
-        
+        System.out.println("** Task 11 **");
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
+            
+        int ar2half = ar2.length/2;
+        System.out.println(ar2half);
+        int task11Temp;
+        for (int i=0 ; i<ar2half ; i++)
+        {
+            task11Temp = ar2[i];
+            System.out.println(" "+i+":"+(ar2.length-i));
+            ar2[i] = ar2[ar2.length-1-i];
+            ar2[ar2.length-1-i]=task11Temp;
+        }
+               
+        for (int i=0 ; i<ar2.length ; i++)
+            System.out.println("ar2["+i+"] = "+ar2[i]);
         
         /*
          * Task 12: 
@@ -257,6 +273,30 @@ class Main
          *  
          *  Count how many words have more than 5 letters.
          */
+        String[] ar5 = new String[i];
+        ar5[0]="Four";
+        ar5[1]="score";
+        ar5[2]="and";
+        ar5[3]="seven";
+        ar5[4]="years";
+        ar5[5]="ago";
+        ar5[6]="our";
+        ar5[7]="fathers";
+        ar5[8]="brought";
+        ar5[9]="forth";
+        ar5[10]="on";
+        ar5[11]="this";
+        ar5[12]="continent";
+        ar5[13]="a";
+        ar5[14]="new";
+        ar5[15]="nation";
+        
+        int len = ar5[i].length();
+        String[] ar6 = ar5;
+        for (int i=0; i<ar5[i].length() ; i++)
+            {
+                
+        
         
         /*
          * Task 13
